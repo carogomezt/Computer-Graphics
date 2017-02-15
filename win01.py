e642ed5f4bbb5f323b9b732b2689e5fce0f785ef
Carolina Gómez Trejos <carolina9511@gmail.com>
2017-02-15 15:46:33 -0500

    Create win01.py

---
+++ win01.py
@@ -0,0 +1,20 @@
+#ven01.py
+from Tkinter import *
+
+def Mostrar():
+    valor = etxt.get()
+    etq2 = Label(ventana, text ="digitado" + valor)
+    etq2.pack()
+
+if __name__ == '__main__':
+    ventana = Tk()
+    ventana.title('ejemplo')
+    etq = Label(ventana, text = "Ejemplo de etiqueta")
+    etxt = Entry(ventana)
+    btn = Button(ventana, text="Aceptar", command = Mostrar)
+    etq2 = Label(ventana, text="digitado: ")
+    etq.pack()
+    etxt.pack()
+    btn.pack()
+    etq2.pack()
+    ventana.mainloop()
