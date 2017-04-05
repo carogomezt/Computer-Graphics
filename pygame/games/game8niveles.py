c5db6f2a2d2baded72607a9ac22c1d09b7b83c44
carogomezt <carolina9511@gmail.com>
2017-04-05 15:56:37 -0500

    levels game

--- pygame/games/game8niveles.py
+++ pygame/games/game8niveles.py
@@ -4,8 +4,8 @@ import ConfigParser
 
 
 
-ANCHO=512
-ALTO=480
+ANCHO=800
+ALTO=600
 BLANCO = (255, 255, 255)
 NEGRO = (0, 0, 0)
 ROJO = (255, 0, 0)
@@ -22,6 +22,8 @@ class Jugador(pygame.sprite.Sprite):
     self.rect = self.image.get_rect()
     self.rect.x = pos[0]
     self.rect.y = pos[1]
+    self.muros = []
+    self.vida = 100
 
   def update(self):
     if self.dir == 2:
@@ -33,45 +35,41 @@ class Jugador(pygame.sprite.Sprite):
     if self.dir == 3:
       self.rect.y -= 2
 
-    self.image = self.matx[self.dir][self.cont] 
-    #self.rect.self.image.get_rect()   
+    self.image = self.matx[self.dir][self.cont]
+    #self.rect.self.image.get_rect()
     if self.cont < 2:
       self.cont += 1
-    else: 
+    else:
       self.cont = 0
+
+    #colision
+    lcol = pygame.sprite.spritecollide(self, muros, False)
+    for e in lcol:
+        self.vida -= 1
+        if self.dir == 2:
+            self.rect.right = e.rect.left
+        if self.dir == 1:
+            self.rect.left = e.rect.right
+        if self.dir == 0:
+            self.rect.bottom = e.rect.top
+        if self.dir == 3:
+            self.rect.top = e.rect.bottom
+
+class Muro(pygame.sprite.Sprite):
+    def __init__(self, obj_img, pos):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = obj_img
+        self.rect = self.image.get_rect()
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
 
 class Nivel(pygame.sprite.Sprite):
     def __init__(self, ar_mapa, an_re, al_re):
       pygame.sprite.Sprite.__init__(self)
-      self.fondo = Traer_fondo(self.ar_fondo, an_re, al_re)
-      self.mapa = Ret_mapa(ar_mapa)
+      self.mapa = self.Ret_mapa(ar_mapa)
+      self.fondo = self.Traer_fondo(self.ar_fondo, an_re, al_re)
 
-    def Ret_mapa(self):
-      archivo = 'mapa.map'
-      mapa = ConfigParser.ConfigParser()
-      mapa.read(archivo)
-      self.ar_fondo=  mapa.get('nivel1', 'origen')
-      plano = mapa.get('nivel1', 'mapa').split('\n')
-      posx = 0
-      posy = 0
-      d = {}
-      for sec in mapa.sections():
-          if len(sec) == 1:
-              # print sec, mapa.get(sec, 'nombre')
-              x = int(mapa.get(sec, 'ux'))
-              y = int(mapa.get(sec, 'uy'))
-              muro= int(mapa.get(sec, 'muro'))
-              d[sec] = [[x, y], muro ] 
-
-      for fila in plano:
-        for e in fila:
-          lista = d[e]
-          if(lista[1] == 1):
-            # TO DO 
-      
-      return d
-
-    def Traer_fondo(archivo, an_re, al_re):
+    def Traer_fondo(self, archivo, an_re, al_re):
       imagen = pygame.image.load('img-maze/background.png')
       ancho, alto = imagen.get_size()
       print ancho, alto
@@ -89,7 +87,39 @@ class Nivel(pygame.sprite.Sprite):
         tabla.append(fila)
       return tabla
 
+    def Ret_mapa(self, archivo):
+      archivo = 'mapa.map'
+      mapa = ConfigParser.ConfigParser()
+      mapa.read(archivo)
+      self.ar_fondo=  mapa.get('nivel1', 'origen')
+      plano = mapa.get('nivel1', 'mapa').split('\n')
+      posx = 0
+      posy = 0
+      d = {}
+      for sec in mapa.sections():
+          if len(sec) == 1:
+              # print sec, mapa.get(sec, 'nombre')
+              x = int(mapa.get(sec, 'ux'))
+              y = int(mapa.get(sec, 'uy'))
+              muro= int(mapa.get(sec, 'muro'))
+              d[sec] = [[x, y], muro ]
 
+      info_muros = []
+      nf = 0
+      for fila in plano:
+        nc = 0
+        print fila
+        for e in fila:
+          lista = d[e]
+          if(lista[1] == 1):
+            pos = [nc, nf]
+            info = [pos, lista[0]]
+            # print info
+            info_muros.append(info)
+          nc += 1
+        nf += 1
+
+      return info_muros
 
 
 if __name__ == '__main__':
@@ -99,7 +129,7 @@ if __name__ == '__main__':
     imagen = pygame.image.load('img-anim/animals.png').convert_alpha()
 
     m = []
-    for fila in range(4):      
+    for fila in range(4):
       lista= []
       for i in range(3):
         cuadro = imagen.subsurface(0+(i*32), 0+(fila*32), 32, 32)
@@ -107,17 +137,37 @@ if __name__ == '__main__':
       m.append(lista)
     jp = Jugador(m,[100, 100])
     todos = pygame.sprite.Group()
+
+    muros = pygame.sprite.Group()
+    mapa = Nivel('mapa.map', 32, 32)
+    for ls in mapa.mapa:
+        pos_p = ls[0]
+        x = pos_p[0]*32
+        y = pos_p[1]*32
+        pos_i = ls[1]
+        xi = pos_i[0]
+        yi = pos_i[1]
+        # print ls[0], x, y
+        m = Muro(mapa.fondo[xi][yi], [x, y])
+        muros.add(m)
+        todos.add(m)
+
+    jp.muros = muros
     todos.add(jp)
+    fondo = pygame.image.load('img-maze/fondo.jpg')
+    fuente = pygame.font.Font(None, 30)
+
 
     fin = False
     reloj = pygame.time.Clock()
     i = 0
+    puntos = 0
+    finjuego = False
     while not fin:
         #Captura de eventos
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 fin = True
-
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
                     jp.dir = 2
@@ -127,12 +177,22 @@ if __name__ == '__main__':
                     jp.dir = 3
                 if event.key == pygame.K_DOWN:
                     jp.dir = 0
-      
 
+        if finjuego:
+            pantalla.fill(NEGRO)
+            texto = fuente.render('GAME OVER', True, BLANCO)
+            pantalla.blit(texto, [250, 200])
+            pygame.display.flip()
 
-        pantalla.fill(NEGRO)
-        todos.update()
-        todos.draw(pantalla)
-        pygame.display.flip()
-        reloj.tick(20)
-
+        else:
+            pantalla.fill(NEGRO)
+            vida = str(jp.vida)
+            if(jp.vida <= 0):
+                finjuego = True
+            texto = fuente.render('Vida: ' + vida, True, NEGRO)
+            pantalla.blit(fondo, [0, 0])
+            pantalla.blit(texto, [20, 435])
+            todos.update()
+            todos.draw(pantalla)
+            pygame.display.flip()
+            reloj.tick(20)
